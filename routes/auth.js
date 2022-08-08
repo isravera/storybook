245a70c6a53f016f914a257dba6fef93c8bca675
Israel Vera <izrael.vb@gmail.com>
2022-08-08 16:39:09 -0400

    added auth middleware and dashboard layout

--- routes/auth.js
+++ routes/auth.js
@@ -10,5 +10,14 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
     res.redirect('/dashboard')
 })
 
+// Logout
+router.get('/logout', (req, res, next) => {
+    req.logout(err => {
+        if(err) {
+            return next(err)
+        }
+    })
+    res.redirect('/')
+})
 
 module.exports = router
